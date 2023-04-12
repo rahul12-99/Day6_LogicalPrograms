9c4ce4982e188d53b8a438fd3430e9cab1bd3268
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-12 09:12:11 +0530

    Added : Welcome message

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    /*
+     * Method to print the welcome message
+     * @param args
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to logical programs!");
+    }
+}
