83ad97f13549aa51bec2d7e30b1fdacd0bdce032
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-04-14 08:40:23 +0530

    Added : Code for check the prime number

--- src/com/logicalprograms/LogicalPrograms.java
+++ src/com/logicalprograms/LogicalPrograms.java
@@ -4,35 +4,34 @@ import java.util.Scanner;
 
 public class LogicalPrograms {
     /*
-     * Method for check the number is perfect or not
+     * Method for check the prime number
      * @param args
      */
     public static void main(String[] args) {
         /*
-         * 1) Taking user input and variables
-         * 2) Taking for loop to get the factor of entered number
-         * 3) check the factor and adding in the sum variable
-         * 4) check if the sum of factor of entered number and entered number
-         * is equal or not and printing
+         * 1) Get user input and initialize  variable
+         * 2) Taking a for loop and getting the factor of entered number
+         * 3) checking the factors and counting the factors
+         * 4) condition for checking prime number or not
          */
-        //  1) Taking user input and variables
+
+        // 1) Get user input and initialize  variable
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
-        int sum = 0;
-        // 2) Taking for loop to get the factor of entered number
-        for (int i = 1; i < n; i++) {
-            // 3) check the factor and adding in the sum variable
-            if(n % i ==0){
-                sum = sum+i;
+        int count = 0;
+        // 2) Taking a for loop and getting the factor of entered number
+        for (int i = 2; i <= n; i++){
+            // 3) checking the factors and counting the factors
+            if(n % i == 0){
+                count++;
             }
         }
-        // 4) check if the sum of factor of entered number and entered number
-        //    is equal or not and printing
-        if(sum == n){
-            System.out.println(n + " is perfect number");
+        // 4) condition for checking prime number or not
+        if(count >= 2){
+            System.out.println(n + " is a prime number");
         }else {
-            System.out.println(n + " is not a perfect number");
+            System.out.println(n + " is not a prime number");
         }
     }
 }
